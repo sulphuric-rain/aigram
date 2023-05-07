f3dbee68b8bc0de6f0dba2f81c29d1400956ed9f
sulphur <thecrapl1945@gmail.com>
2023-05-07 15:35:32 +0300

    chat interaction

---
+++ chat_ia.py
@@ -0,0 +1,14 @@
+from tgconfig import ai_key
+import requests
+
+
+def send_rq_to_chat(request_text):
+    URL_CHAT = "https://api.openai.com/v1/chat/completions"
+    headers = {"Content-Type:" : "application/json", "Authorization: ":f"Bearer {ai_key}"}
+    payload = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": f"{request_text}"}]
+    }
+    response = requests.post(URL_CHAT, headers=headers, json=payload)
+    return response.json().get("chices")[0].get("message").get("content")
+
